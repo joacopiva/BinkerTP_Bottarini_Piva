79088e534d0515c37105e2046e98b58543d612b8
JOAQUIN PIVA <47870976@ort>
2024-05-22 08:47:51 -0300

    terminando conocimientos

--- my-app/src/components/IconosConocimientos/index.jsx
+++ my-app/src/components/IconosConocimientos/index.jsx
@@ -1,8 +1,8 @@
 let IconosConocimientos = ({url, texto}) =>
 (
-    <div style={{margin: "10", justifyContent: "center"}}>
-        <img src={url} alt="..."  style={{padding: "1vw", width: "25%", height: "25%", marginLeft: "8vw", height: "7vw"}}></img>
-        <p style={{paddingLeft: "17vw",fontSize: "large", }}> {texto}</p>
+    <div style={{margin: "10", textAlign: "center", justifyContent: "space-between", margin: "4vw", paddingLeft: "6vw", marginTop: "3vw", marginBottom: "5vw"}}>
+        <img src={url} alt="..."  style={{ width: "7vw", height: "7vw"}}></img>
+        <p style={{fontSize: "large"}}> {texto}</p>
     </div>
 
 )
--- my-app/src/views/Conocimientos/index.jsx
+++ my-app/src/views/Conocimientos/index.jsx
@@ -1,14 +1,53 @@
 import IconosConocimientos from "../../components/IconosConocimientos"
+import Titulo from "../../components/Titulo"
 
 let Conocimientos = () =>
 {
     const url1 = "https://ezebinker.github.io/images/services/web-programming.svg"
     const texto1 = "Desarrollo Web"
+    const url2 = "https://ezebinker.github.io/images/services/app-development.svg"
+    const texto2 = "Desarrollo Móbil"
+    const url3 = "https://ezebinker.github.io/images/services/blockchain.svg"
+    const texto3 = "Blockchain & Criptomonedas"
+    const url4 = "https://ezebinker.github.io/images/services/ui-ux.svg"
+    const texto4 = "UX/UI"
     return(
-        <IconosConocimientos
-        url = {url1}
-        texto = {texto1}
-        />
+        <section id="#conocimientos">
+            <>
+            <Titulo
+            texto = {"Conocimientos"}
+            />
+            
+
+            <strong>
+            <div style={{display: "flex"}}>
+                
+            <IconosConocimientos
+            url = {url1}
+            texto = {texto1}
+            />
+            
+            <IconosConocimientos
+            url = {url2}
+            texto = {texto2}
+            />
+
+            <IconosConocimientos
+            url = {url3}
+            texto = {texto3}
+            />
+            <IconosConocimientos
+            url = {url4}
+            texto = {texto4}
+            />
+            </div>
+            </strong>
+            </>
+        </section>
+        
+        
+        
+        
     )   
 }
 export default Conocimientos
